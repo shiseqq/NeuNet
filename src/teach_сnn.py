567bb3ca9e7787f308bfdf8d93ab325ae465c89a
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-04-28 22:28:30 +0700

    Implement learning algorithm cnn

--- src/teach_сnn.py
+++ src/teach_сnn.py
@@ -20,3 +20,22 @@ model.add(Dense(10, activation = 'softmax'))
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 print(model.summary())
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+model.fit(x_train, y_train, batch_size=200, epochs=10, validation_split=0.2, verbose=1)
+
+score = model.evaluate(x_test, y_test, verbose = 0)
+print("Точность на тестовых наборах: %.2f%%" % (score[1]*100))
+
