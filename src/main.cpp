f4481c3c5aa94a598f8acb14dec1730fae2411f7
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-05-11 00:02:59 +0700

    Set relative import for python

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <python3.6/Python.h>
+
+using namespace std;
+
+int main()
+{
+    Py_Initialize();
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("import os");
+    PyRun_SimpleString("sys.path.insert(1, \"../src\")");
+    return 0;
+}
