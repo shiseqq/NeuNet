470fc28eb2753fe8d44a610903f0a950b4ba711e
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-05-10 23:44:08 +0700

    Algorithm from file image_analysis convert to function

--- src/image_analysis.py
+++ src/image_analysis.py
@@ -1,34 +1,24 @@
 import numpy as np
-from keras.models import model_from_json
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
-import sys
-import os
+from keras.models import model_from_json
 
-if os.path.exists("../CNN/model.json") and os.path.exists("../CNN/model_weights.h5"):
+
+def analysis(img_path):
     fl = open("../CNN/model.json", "r")
     json = fl.read()
     fl.close()
     model = model_from_json(json)
     model.load_weights("../CNN/model_weights.h5")
-else:
-    print("Missing file model.json and / or file model_weights.h5 in the folder CNN. Please run training сnn.")
-    os.abort()
+    model.compile(loss="categorical_crossentropy",
+                  optimizer="adam", metrics=["accuracy"])
+    img = image.load_img(img_path, target_size=(
+        28, 28), color_mode='grayscale')
 
-model.compile(loss="categorical_crossentropy",
-              optimizer="adam", metrics=["accuracy"])
+    x = image.img_to_array(img)
+    x = 255 - x
+    x /= 255
+    x = np.expand_dims(x, axis=0)
 
-
-img_path = input("Enter the path to the image: ")
-img = image.load_img(img_path, target_size=(28, 28), color_mode='grayscale')
-
-x = image.img_to_array(img)
-x = 255 - x
-x /= 255
-x = np.expand_dims(x, axis=0)
-
-prd = model.predict(x)
-prd = np.argmax(prd, axis=1)
-print(int(prd))
-plt.imshow(img, cmap='gray')
-plt.show()
+    prd = model.predict(x)
+    prd = np.argmax(prd, axis=1)
+    return int(prd)
