c64b055ad2d21e690b3d7095c209d97b7140659e
Igor(shise) <1.mer96@mail.ru>
2019-05-20 08:30:58 +0700

    Add button for select image

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <chrono>
+#include <iostream>
 #include <thread>
 
 using namespace sf;
@@ -15,12 +16,16 @@ int main()
     t1.setFillColor(Color(173, 255, 47));
     t1.setPosition(100, 930);
     t1.setOutlineThickness(1);
+    Text but1txt("Select image", font, 50);
+    but1txt.setFillColor(Color::White);
+    but1txt.setPosition(
+            window.getSize().x / 2 - 150, window.getSize().y / 2 - 165);
     Texture bg0;
     bg0.loadFromFile("../texture/FS_30.jpg");
     Sprite bg;
     bg.setTexture(bg0);
     Texture but0;
-    but0.loadFromFile("../texture/button.png");
+    but0.loadFromFile("button.png");
     Sprite button1;
     button1.setTexture(but0);
     button1.setPosition(
@@ -30,9 +35,15 @@ int main()
         if (Keyboard::isKeyPressed(Keyboard::Escape)) {
             exit(0);
         }
+        if (Mouse::isButtonPressed(Mouse::Left)
+            && IntRect(678, 350, but0.getSize().x, but0.getSize().y)
+                       .contains(Mouse::getPosition(window))) {
+            exit(0);
+        }
         window.clear();
         window.draw(bg);
         window.draw(button1);
+        window.draw(but1txt);
         window.draw(t1);
         window.display();
     }
