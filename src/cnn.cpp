b4c9abd8a39b807333bce3a64fe15cdaa2bd9f63
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-05-24 15:32:35 +0700

    Add clear memory allocated for Python objects in cnn functions

--- src/cnn.cpp
+++ src/cnn.cpp
@@ -5,7 +5,7 @@ void teach()
     PyObject *pArgs, *pModule, *pFunc, *pName;
     pName = PyUnicode_FromString("teach_сnn");
     pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
+    Py_XDECREF(pName);
     if (pModule) {
         pFunc = PyObject_GetAttrString(pModule, "teach");
         if (pFunc) {
@@ -13,9 +13,9 @@ void teach()
             PyObject_CallObject(pFunc, pArgs);
         }
     }
-    Py_DECREF(pModule);
-    Py_DECREF(pArgs);
-    Py_DECREF(pFunc);
+    Py_XDECREF(pModule);
+    Py_XDECREF(pArgs);
+    Py_XDECREF(pFunc);
     Py_Finalize();
 }
 
@@ -24,15 +24,21 @@ int analysis(string str, PyObject*& model)
     PyObject *pName, *pModule, *pFunc, *pValue, *pArgs;
     pName = PyUnicode_FromString("image_analysis");
     pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
+    Py_XDECREF(pName);
     if (pModule) {
         pFunc = PyObject_GetAttrString(pModule, "analysis");
         pArgs = PyTuple_New(2);
         PyTuple_SetItem(pArgs, 0, PyUnicode_FromString(str.c_str()));
         PyTuple_SetItem(pArgs, 1, model);
         pValue = PyObject_CallObject(pFunc, pArgs);
+        Py_XDECREF(pModule);
+        Py_XDECREF(pFunc);
+        Py_XDECREF(pArgs);
         return PyLong_AsLong(pValue);
     }
+    Py_XDECREF(pModule);
+    Py_XDECREF(pFunc);
+    Py_XDECREF(pArgs);
     Py_Finalize();
     return -1;
 }
@@ -42,12 +48,15 @@ void startcnn(PyObject*& model)
     PyObject *pName, *pModule, *pFunc, *pArgs;
     pName = PyUnicode_FromString("startcnn");
     pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
+    Py_XDECREF(pName);
     if (pModule) {
         pFunc = PyObject_GetAttrString(pModule, "startcnn");
         pArgs = PyTuple_New(0);
         model = PyObject_CallObject(pFunc, pArgs);
     }
+    Py_XDECREF(pModule);
+    Py_XDECREF(pFunc);
+    Py_XDECREF(pArgs);
 }
 
 void lerarnpicture(string img_path, int target, PyObject*& model)
@@ -55,7 +64,7 @@ void lerarnpicture(string img_path, int target, PyObject*& model)
     PyObject *pName, *pModule, *pFunc, *pArgs;
     pName = PyUnicode_FromString("learn_picture");
     pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
+    Py_XDECREF(pName);
     if (pModule) {
         pFunc = PyObject_GetAttrString(pModule, "lerarnpicture");
         pArgs = PyTuple_New(3);
@@ -64,7 +73,7 @@ void lerarnpicture(string img_path, int target, PyObject*& model)
         PyTuple_SetItem(pArgs, 2, model);
         PyObject_CallObject(pFunc, pArgs);
     }
-    Py_DECREF(pModule);
-    Py_DECREF(pFunc);
-    Py_DECREF(pArgs);
+    Py_XDECREF(pModule);
+    Py_XDECREF(pFunc);
+    Py_XDECREF(pArgs);
 }
