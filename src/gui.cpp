2ba861456ecd64fc834d028d2bc9ff01a2aa778b
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-05-26 13:22:16 +0700

    Associate a c++ function 'wininp' with a python function 'wininp'

--- src/gui.cpp
+++ src/gui.cpp
@@ -24,3 +24,24 @@ string selectfile()
         return "";
     }
 }
+
+int wininp()
+{
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.argv = ['']");
+    PyObject *pArgs, *pModule, *pFunc, *pName, *pValue;
+    pName = PyUnicode_FromString("gui");
+    pModule = PyImport_Import(pName);
+    Py_XDECREF(pName);
+    if (pModule) {
+        pFunc = PyObject_GetAttrString(pModule, "wininp");
+        if (pFunc) {
+            pArgs = PyTuple_New(0);
+            pValue = PyObject_CallObject(pFunc, pArgs);
+        }
+    }
+    Py_XDECREF(pModule);
+    Py_XDECREF(pArgs);
+    Py_XDECREF(pFunc);
+    return PyLong_AsLong(pValue);
+}
--- src/gui.h
+++ src/gui.h
@@ -6,5 +6,6 @@
 using namespace std;
 
 string selectfile();
+int wininp();
 
 #endif
