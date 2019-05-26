6f95afd892e4d61c956c857f1371db2ad925cc92
Maxim Kapustin <dragonxxl2016@yandex.ru>
2019-05-26 13:15:18 +0700

    Implement function for digit input box

--- src/gui.py
+++ src/gui.py
@@ -11,3 +11,32 @@ def selectfile():
                                    initialdir=os.path.expanduser("~"), filetypes=[("PNG files", "*.png")])
     root.destroy()
     return file_path
+
+
+def wininp():
+    s = ''
+
+    def check(event):
+        nonlocal s
+        s = entry.get()
+        if not s.isdigit() or int(s) > 9:
+            tk.Tk().withdraw()
+            mb.showerror("Ошибка", "Должна быть введена цифра")
+        else:
+            win.destroy()
+
+    win = tk.Tk()
+    win.title(" ")
+    win.resizable(False, False)
+    win.geometry("200x100")
+    lable = tk.Label(win, height=1, text="Введите цифру").pack()
+    entry = tk.Entry(win, width=20)
+    entry.pack(pady=7)
+    tk.Button(win, text="ОК", command=lambda: check(0)).pack()
+    win.bind("<Return>", check)
+    win.mainloop()
+
+    if not s.isdigit() or int(s) > 9:
+        return -1
+    else:
+        return int(s)
