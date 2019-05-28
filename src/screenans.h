57a4be1c28568bf7674ed01243dcf6f37906ef23
Igor(shise) <1.mer96@mail.ru>
2019-05-28 15:42:18 +0700

    Add screen for anwer

---
+++ src/screenans.h
@@ -0,0 +1,12 @@
+#ifndef _SCREENANS_H
+#define _SCREENANS_H
+#include <SFML/Graphics.hpp>
+#include <python3.6/Python.h>
+#include <string>
+
+using namespace std;
+using namespace sf;
+
+void screenans(RenderWindow* window, int t);
+
+#endif
